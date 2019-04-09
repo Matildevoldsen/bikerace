cd1a78d46821cfb3034a32a0f1346cc782829d40
Matilde Wittrup Enevoldsen <matildeene@outlook.com>
2019-04-09 18:36:07 +0100

    Add support for .env files using env-cmd.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,10 @@
 import React, { Component } from 'react';
-import './App.css';
 
 class App extends Component {
+  componentDidMount() {
+    document.title = process.env.REACT_APP_NAME + " - Home";
+  }
+
   render() {
     return (
       <div className="App">
