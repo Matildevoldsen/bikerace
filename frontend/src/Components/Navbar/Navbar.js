d4652408f1e849fed712f382aa5317e7b62841bd
Matilde Wittrup Enevoldsen <matildeene@outlook.com>
2019-04-11 19:04:07 +0100

    Add fonts and style navigation bar.

--- frontend/src/Components/Navbar/Navbar.js
+++ frontend/src/Components/Navbar/Navbar.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import {faBars} from '@fortawesome/free-solid-svg-icons'
 import {Link} from 'react-router-dom';
+import './Navbar.css';
 
 export default class Navbar extends Component {
     constructor(e) {
